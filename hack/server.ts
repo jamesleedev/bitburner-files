2b299d6f55ca944bad3bf62b341c3611c0dee1f6
James Lee <james@jamesl.dev>
2026-01-23 00:37:07 +0000

    chore: refactor hack/server.ts to use getHostFlag

--- hack/server.ts
+++ hack/server.ts
@@ -1,12 +1,16 @@
+import { getHostFlag } from "../utils/flags";
+
 export async function main(ns: NS) {
-  if (ns.args.length === 0) {
-    ns.tprint('ERROR missing network node argument')
+  const cmdFlags = ns.flags([
+    ["host", ""],
+    ["h", ""],
+  ]);
+
+  const target = getHostFlag(ns, cmdFlags, true, true);
+
+  if (!target) {
     return;
   }
-
-  // Defines the "target server", which is the server
-  // that we're going to hack. In this case, it's "n00dles"
-  const target = ns.args[0].toString() as string;
 
   // Defines how much money a server should have before we hack it
   // In this case, it is set to the maximum amount of money.
@@ -27,16 +31,16 @@ export async function main(ns: NS) {
   //ns.nuke(target);
 
   // Infinite loop that continously hacks/grows/weakens the target server
-  while(true) {
-      if (ns.getServerSecurityLevel(target) > securityThresh) {
-          // If the server's security level is above our threshold, weaken it
-          await ns.weaken(target);
-      } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-          // If the server's money is less than our threshold, grow it
-          await ns.grow(target);
-      } else {
-          // Otherwise, hack it
-          await ns.hack(target);
-      }
+  while (true) {
+    if (ns.getServerSecurityLevel(target) > securityThresh) {
+      // If the server's security level is above our threshold, weaken it
+      await ns.weaken(target);
+    } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+      // If the server's money is less than our threshold, grow it
+      await ns.grow(target);
+    } else {
+      // Otherwise, hack it
+      await ns.hack(target);
+    }
   }
 }
