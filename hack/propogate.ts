f5885d7bdef7ade0c8ceac55737f9cfff6df5bd8
James Lee <james@jamesl.dev>
2026-01-26 14:50:12 +0000

    chore: add flag to include home

--- hack/propogate.ts
+++ hack/propogate.ts
@@ -7,6 +7,7 @@ const FLAGS: Flags = [
   ['depth', 1],
   ['host', ''],
   ['kill', false],
+  ['home', false],
 ];
 
 export function autocomplete(data: AutocompleteData, args: string[]): string[] {
@@ -28,8 +29,17 @@ export async function main(ns: NS) {
   const depth = cmdFlags.depth as number;
   const singleHost = cmdFlags.host as string;
   const killAll = cmdFlags.kill as boolean;
+  const includeHome = cmdFlags.home as boolean;
 
-  const nodes = singleHost && singleHost !== '' ? [singleHost] : getHost(ns, depth);
+  let nodes = singleHost && singleHost !== '' ? [singleHost] : getHost(ns, depth);
+
+  nodes = nodes.filter((host) => {
+    if (!includeHome && host === 'home') {
+      return false;
+    }
+
+    return ns.hasRootAccess(host);
+  });
 
   if (!scriptName) {
     ns.tprint('ERROR missing script name to propogate');
@@ -46,36 +56,34 @@ export async function main(ns: NS) {
   let totalThreads = 0;
 
   for (const host of nodes) {
-    if (ns.hasRootAccess(host) && host !== 'home') {
-      let instances: number = 0;
-      const scriptRam = ns.getScriptRam(scriptName);
-      const server: Record<string, any> = {
-        maxRam: ns.getServerMaxRam(host),
-        usedRam: ns.getServerUsedRam(host),
-      };
-      server.freeRam = server.maxRam - server.usedRam;
+    let instances: number = 0;
+    const scriptRam = ns.getScriptRam(scriptName);
+    const server: Record<string, any> = {
+      maxRam: ns.getServerMaxRam(host),
+      usedRam: ns.getServerUsedRam(host),
+    };
+    server.freeRam = server.maxRam - server.usedRam;
 
-      //ns.tprint(`INFO Host: ${host}`)
-      //ns.tprint(`INFO Instances: ${instances}`)
-      //ns.tprint(`INFO Host Max RAM: ${ns.getServerMaxRam(host)}`)
-      //ns.tprint(`INFO Script RAM: ${ns.getScriptRam(scriptName)}`)
+    //ns.tprint(`INFO Host: ${host}`)
+    //ns.tprint(`INFO Instances: ${instances}`)
+    //ns.tprint(`INFO Host Max RAM: ${ns.getServerMaxRam(host)}`)
+    //ns.tprint(`INFO Script RAM: ${ns.getScriptRam(scriptName)}`)
 
-      if (killAll) {
-        ns.killall(host);
-        ns.tprint(`SUCCESS ${host}: killing all scripts`);
-        instances = Math.floor(server.maxRam / scriptRam);
-      } else {
-        instances = Math.floor(server.freeRam / scriptRam);
-      }
+    if (killAll) {
+      ns.killall(host);
+      ns.tprint(`SUCCESS ${host}: killing all scripts`);
+      instances = Math.floor(server.maxRam / scriptRam);
+    } else {
+      instances = Math.floor(server.freeRam / scriptRam);
+    }
 
-      ns.scp(scriptName, host);
-      ns.tprint(`SUCCESS ${host}: copying latest script`);
+    ns.scp(scriptName, host);
+    ns.tprint(`SUCCESS ${host}: copying latest script`);
 
-      if (instances > 0) {
-        ns.exec(scriptName, host, instances, target);
-        ns.tprint(`SUCCESS ${host}: Started ${instances} thread(s) of script ${scriptName} with args of [${target}].`);
-        totalThreads += instances;
-      }
+    if (instances > 0) {
+      ns.exec(scriptName, host, instances, target);
+      ns.tprint(`SUCCESS ${host}: Started ${instances} thread(s) of script ${scriptName} with args of [${target}].`);
+      totalThreads += instances;
     }
   }
 
